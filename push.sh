e0adc4360304ed4be94ce4d031b12e457825971d
zhangcy <zcydev@aliyun.com>
2016-08-16 19:31:00 +0800

    更新时间2016-08-16 11:31:00

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
+git pull
 git add .
 git commit -m "更新时间$(date "+%Y-%m-%d %H:%M:%S")"
 git push
---
+++ rocedar/fz_up_nginx.sh
@@ -0,0 +1,3 @@
+scp -r /d/huanyi/fz_server/hydevice/src/main/webapp/static/css/device nginx@192.168.18.21:/home/nginx/html/static/css 
+
+scp -r /d/huanyi/fz_server/hydevice/src/main/webapp/static/js/device nginx@192.168.18.21:/home/nginx/html/static/js
